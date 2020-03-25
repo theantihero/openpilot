f467d609698d8cb77118c9a527db192ac01eb1a9
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-03-25 03:00:02 -0400

    DUITUNE

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -16,7 +16,7 @@ extern "C"{
 #include "common/glutil.h"
 }
 
-int border_shifter = 22; //Use this to move elements around depending on how much bdr_s is changed -wirelessnet2
+int border_shifter = 20; //Use this to move elements around depending on how much bdr_s is changed -wirelessnet2
 
 // TODO: this is also hardcoded in common/transformations/camera.py
 const mat3 intrinsic_matrix = (mat3){{
@@ -659,7 +659,7 @@ static void ui_draw_vision_event(UIState *s) {
   const int viz_event_h = (header_h - (bdr_is*1.5));
   if (s->scene.decel_for_model && s->scene.engaged) {
     // draw winding road sign
-    const int img_turn_size = 160*1.5;
+    const int img_turn_size = 160*1.5*0.9375; //.9375 is the same factor I scaled the steering wheel image by -wirelessnet2
     const int img_turn_x = viz_event_x-(img_turn_size/4);
     const int img_turn_y = viz_event_y+bdr_is-25;
     float img_turn_alpha = 1.0f;
@@ -671,7 +671,7 @@ static void ui_draw_vision_event(UIState *s) {
     nvgFill(s->vg);
   } else {
     // draw steering wheel
-    const int bg_wheel_size = 96;
+    const int bg_wheel_size = 90; //I made the wheel a bit smaller -wirelessnet2
     const int bg_wheel_x = viz_event_x + (viz_event_w-bg_wheel_size);
     const int bg_wheel_y = viz_event_y + (bg_wheel_size/2);
     const int img_wheel_size = bg_wheel_size*1.5;
@@ -737,7 +737,7 @@ static void ui_draw_vision_face(UIState *s) {
   const int face_y = (footer_y + ((footer_h - face_size) / 2));
   const int face_img_size = (face_size * 1.5);
   const int face_img_x = (face_x - (face_img_size / 2));
-  const int face_img_y = (face_y - (face_size / 4)+border_shifter); //Move the DM face with the border width -wirelessnet2
+  const int face_img_y = (face_y - (face_size / 4)+border_shifter+5); //Move the DM face with the border width -wirelessnet2
   float face_img_alpha = scene->monitoring_active ? 1.0f : 0.15f;
   float face_bg_alpha = scene->monitoring_active ? 0.3f : 0.1f;
   NVGcolor face_bg = nvgRGBA(0, 0, 0, (255 * face_bg_alpha));
@@ -745,7 +745,7 @@ static void ui_draw_vision_face(UIState *s) {
     face_img_size, face_img_size, 0, s->img_face, face_img_alpha);
 
   nvgBeginPath(s->vg);
-  nvgCircle(s->vg, face_x, (face_y + (bdr_is * 1.5)+border_shifter), face_size);
+  nvgCircle(s->vg, face_x, (face_y + (bdr_is * 1.5)+border_shifter+5), face_size);
   nvgFillColor(s->vg, face_bg);
   nvgFill(s->vg);
 
@@ -758,11 +758,11 @@ static void ui_draw_vision_face(UIState *s) {
 static void ui_draw_vision_brake(UIState *s) {
   const UIScene *scene = &s->scene;
   const int brake_size = 85; //made the Brake Icon a bit smaller -wirelessnet2
-  const int brake_x = (scene->ui_viz_rx + (brake_size * 5) + (bdr_is * 3));
+  const int brake_x = (scene->ui_viz_rx + (brake_size * 5) + (bdr_is * 2.5)); //Moved brake icon a bit closer to DM icon -wirelessnet2
   const int brake_y = (footer_y + ((footer_h - brake_size) / 2));
   const int brake_img_size = (brake_size * 1.5);
   const int brake_img_x = (brake_x - (brake_img_size / 2));
-  const int brake_img_y = (brake_y - (brake_size / 4)+border_shifter);
+  const int brake_img_y = (brake_y - (brake_size / 4)+border_shifter+5);
 
   bool brake_valid = scene->brakeLights;
   float brake_img_alpha = brake_valid ? 1.0f : 0.15f;
@@ -772,7 +772,7 @@ static void ui_draw_vision_brake(UIState *s) {
     brake_img_size, brake_img_size, 0, s->img_brake, brake_img_alpha);
 
   nvgBeginPath(s->vg);
-  nvgCircle(s->vg, brake_x, (brake_y + (bdr_is * 1.5)+border_shifter), brake_size);
+  nvgCircle(s->vg, brake_x, (brake_y + (bdr_is * 1.5)+border_shifter+5), brake_size);
   nvgFillColor(s->vg, brake_bg);
   nvgFill(s->vg);
 
