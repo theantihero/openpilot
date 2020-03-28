c9d5b05b23ffca2464e97f52de3a658040092bb9
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-03-27 20:11:54 -0400

    ._.

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -878,6 +878,7 @@ static void bb_ui_draw_measures_left(UIState *s, int bb_x, int bb_y, int bb_w ) 
         val_color, lab_color, uom_color,
         value_fontSize, label_fontSize, uom_fontSize );
     bb_ry = bb_y + bb_h;
+    }
   //add Ublox GPS accuracy
   if (scene->gpsAccuracyUblox != 0.00) {
     char val_str[16];
