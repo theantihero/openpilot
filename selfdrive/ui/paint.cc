460bc998889701edb1c6512501429348ead0a915
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-03-28 21:02:34 -0400

    Tune

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -661,7 +661,7 @@ static void ui_draw_vision_event(UIState *s) {
     // draw winding road sign
     const int img_turn_size = 160*1.5*0.82; //.9375 is the same factor I scaled the steering wheel image by -wirelessnet2
     const int img_turn_x = viz_event_x-(img_turn_size/4)+80;
-    const int img_turn_y = viz_event_y+bdr_is-55;
+    const int img_turn_y = viz_event_y+bdr_is-45;
     float img_turn_alpha = 1.0f;
     nvgBeginPath(s->vg);
     NVGpaint imgPaint = nvgImagePattern(s->vg, img_turn_x, img_turn_y,
