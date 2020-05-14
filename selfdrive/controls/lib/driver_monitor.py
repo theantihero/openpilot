7ffff421f5f790325f642578f1dfdd82ea0434cf
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2020-05-14 02:54:01 -0400

    2x DM Time Limits

--- selfdrive/controls/lib/driver_monitor.py
+++ selfdrive/controls/lib/driver_monitor.py
@@ -11,12 +11,12 @@ from common.stat_live import RunningStatFilter
 #  We recommend that you do not change these numbers from the defaults.
 # ******************************************************************************************
 
-_AWARENESS_TIME = 70.  # one minute limit without user touching steering wheels make the car enter a terminal status
-_AWARENESS_PRE_TIME_TILL_TERMINAL = 15.  # a first alert is issued 25s before expiration
-_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.  # a second alert is issued 15s before start decelerating the car
-_DISTRACTED_TIME = 11.
-_DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
-_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
+_AWARENESS_TIME = 140.  # one minute limit without user touching steering wheels make the car enter a terminal status
+_AWARENESS_PRE_TIME_TILL_TERMINAL = 30.  # a first alert is issued 25s before expiration
+_AWARENESS_PROMPT_TIME_TILL_TERMINAL = 12.  # a second alert is issued 15s before start decelerating the car
+_DISTRACTED_TIME = 22.
+_DISTRACTED_PRE_TIME_TILL_TERMINAL = 16.
+_DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 12.
 
 _FACE_THRESHOLD = 0.4
 _EYE_THRESHOLD = 0.6
